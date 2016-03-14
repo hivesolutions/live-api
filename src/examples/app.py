2e45e63d8ee80f362d614bee9f86476bca95a713
joamag <joamag@gmail.com>
2016-03-14 23:54:57 +0000

    multiple kwargs in apps

--- src/examples/app.py
+++ src/examples/app.py
@@ -43,8 +43,12 @@ from . import base
 
 class LiveApp(appier.WebApp):
 
-    def __init__(self):
-        appier.WebApp.__init__(self, name = "live")
+    def __init__(self, *args, **kwargs):
+        appier.WebApp.__init__(
+            self,
+            name = "live",
+            *args, **kwargs
+        )
 
     @appier.route("/", "GET")
     def index(self):
